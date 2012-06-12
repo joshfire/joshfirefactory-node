5051767db19aafe6b3e3d5d8f7d2b6be76015161
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2012-06-12 12:47:19 +0200

    Working!

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var fs = require("fs"),
     vm = require('vm'),
     requirejs = require("requirejs");
 
+var packagejson = JSON.parse(fs.readFileSync(__dirname+"/package.json","utf-8").replace(/\n/g,""));
+
 var datajs_client = require("./lib/datajs.client"),
 	datajs_proxy = require("./lib/datajs.proxy"),
 	getdatasource = require("./lib/getdatasource");
@@ -25,10 +27,11 @@ var init = function(Joshfire) {
 exports.bootstrap = function(options,callback) {
 
   var fetch = function(cb) {
-    var parsed = fs.readFile(__dirname+"/joshfire_factory_boostrap.js","utf-8",function(err,data) {
+    var parsed = fs.readFile("joshfire_factory_boostrap.js","utf-8",function(err,data) {
       if (!err) {
-        return callback(null,data);
+        return cb(null,data);
       }
+
 
       var appid = options.appid || process.env.JOSHFIREFACTORY_APPID;
       var templateid = options.templateid || process.env.JOSHFIREFACTORY_TEMPLATEID || "auto";
@@ -39,11 +42,13 @@ exports.bootstrap = function(options,callback) {
         return cb("no static bootstrap and no ID given - impossible to boostrap");
       }
 
-      request({uri:"http://"+host+"/boostrap/"+appid+"/"+templateid+"/"+deviceid+"/?runtime=nodejs"},function(err,resp,body) {
+      request({uri:"http://"+host+"/bootstrap/"+appid+"/"+templateid+"/"+deviceid+"/?runtime=nodejs&runtimev="+packagejson.version},function(err,resp,body) {
         if (err) return cb(err);
 
         if (options.save) {
-          fs.writeFile(__dirname+"/joshfire_factory_boostrap.js","utf-8",body);
+          fs.writeFile("joshfire_factory_boostrap.js",body,"utf-8",function(err) {
+
+          });
         }
 
         try {
