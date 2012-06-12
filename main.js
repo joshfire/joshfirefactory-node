e22b478ee842ff1226fcc2f1d0fa100e227ed86a
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2012-06-12 11:01:43 +0200

    almost stable

--- main.js
+++ main.js
@@ -1,27 +1,33 @@
 var fs = require("fs"),
-    request = require("request");
+    request = require("request"),
+    vm = require('vm'),
+    requirejs = require("requirejs");
 
 var datajs_client = require("./lib/datajs.client"),
 	datajs_proxy = require("./lib/datajs.proxy"),
 	getdatasource = require("./lib/getdatasource");
 
-
+requirejs.config({
+  baseUrl: __dirname,
+  nodeRequire: require,
+  context:"datajsrequire"
+});
 
 var init = function(Joshfire) {
   Joshfire.datajs = {
-    client:datajs_client.init(Joshfire),
+    client:datajs_client.init(Joshfire, requirejs),
     proxy:datajs_proxy.init(Joshfire)
   };
   Joshfire.factory.getDataSource = getdatasource.init(Joshfire);
 };
 
 
-var bootstrap = function(options,callback) {
+exports.bootstrap = function(options,callback) {
 
   var fetch = function(cb) {
-    var parsed = fs.readFile(__dirname+"/joshfire_factory_boostrap.json","utf-8",function(err,data) {
+    var parsed = fs.readFile(__dirname+"/joshfire_factory_boostrap.js","utf-8",function(err,data) {
       if (!err) {
-        return callback(null,JSON.parse(data));
+        return callback(null,data);
       }
 
       var appid = options.appid || process.env.JOSHFIREFACTORY_APPID;
@@ -35,8 +41,13 @@ var bootstrap = function(options,callback) {
 
       request({uri:"http://"+host+"/boostrap/"+appid+"/"+templateid+"/"+deviceid+"/?runtime=nodejs"},function(err,resp,body) {
         if (err) return cb(err);
+
+        if (options.save) {
+          fs.writeFile(__dirname+"/joshfire_factory_boostrap.js","utf-8",body);
+        }
+
         try {
-          return cb(null,JSON.parse(body));
+          return cb(null,body);
         } catch (e) {
           return cb("JSON parse error");
         }
@@ -46,37 +57,23 @@ var bootstrap = function(options,callback) {
     });
   };
 
-  fetch(function(err, factoryjson) {
+  fetch(function(err, factoryjs) {
     if (err) return callback(err);
-    var J = {
-      factory:factoryjson
+
+    var sandbox = {
+      Joshfire:{},
+      define:requirejs.define
     };
-    init(J);
-    callback(null,J);
+
+    try {
+      vm.runInNewContext(factoryjs, sandbox, 'joshfire_factory_boostrap.vm.js');
+    } catch (e) {
+      return callback(e);
+    }
+    
+    init(sandbox.Joshfire);
+
+    callback(null,sandbox.Joshfire);
   });
 
-  
 };
-
-
-
-
-
-global.define = requirejs.define;
-require("./datajs_datasources.js");
-global.define = null;
-
-/* Client shim! */
-var dbrequire = requirejs.config({
-  baseUrl: __dirname,
-  nodeRequire: require,
-  context:"dbrequire"
-});
-
-
-  var uri = "http://"+Joshfire.factory.globalConfig.DATAHOSTPORT+"/api/" + Joshfire.factory.globalConfig.DATAVERSION + "/_build?"+querystring.encode({"runtime":"nodejs","collections":JSON.stringify(_.keys(datajslib))});
-  console.log("Fetching datasources at",uri);
-  request({uri: uri},function(err,response,code) {
-    if (err) return cb(err);
-    fs.writeFile(__dirname+"/datajs_datasources.js",code,"utf-8",cb);
-  });
